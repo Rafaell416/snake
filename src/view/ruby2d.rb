e3b38a27c201a1715f7725c89eac2af2c612d8da
Rafel Villarreal <rvillarreal416@gmail.com>
2019-03-27 11:24:09 -0500

    render food and initial snake

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -7,15 +7,43 @@ module View
       @pixel_size = 50 
     end
 
-    def render(state)
+    def get_dsl
       extend Ruby2D::DSL
+    end
+
+    def render state
+      get_dsl
       set(
         title: 'Snake', 
         width: @pixel_size * state.grid.cols, 
         height: @pixel_size * state.grid.rows
       )
+      render_snake(state)
+      render_food(state)
       show
     end
 
+    private
+      def render_snake state
+        get_dsl
+        snake = state.snake
+        Square.new(
+          x: snake.positions[1].row * @pixel_size,
+          y: snake.positions[1].row * @pixel_size,
+          size: @pixel_size,
+          color: 'green'
+        )
+      end
+
+      def render_food state
+        get_dsl
+        food = state.food
+        Square.new(
+          x: food.col * @pixel_size,
+          y: food.row * @pixel_size,
+          size: @pixel_size,
+          color: 'yellow',
+        )
+      end
   end
 end
